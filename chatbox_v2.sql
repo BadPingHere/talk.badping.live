d702aab8384db9e3ba6e9bcba765299652ad58e4
BadPingHere <62813557+BadPingHere@users.noreply.github.com>
2022-12-19 19:22:36 -0500

    add sql file!

--- chatbox_v2.sql
+++ chatbox_v2.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 4.9.7
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jul 26, 2022 at 03:25 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
+-- Host: localhost:3306
+-- Generation Time: Dec 19, 2022 at 07:21 PM
+-- Server version: 10.3.37-MariaDB-log-cll-lve
+-- PHP Version: 7.4.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,42 +19,43 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `registered`
+-- Database: `badprtbv_chatbox`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `userst`
+-- Table structure for table `chatbox_v2`
 --
 
-CREATE TABLE `userst` (
-  `id` int(11) NOT NULL,
+CREATE TABLE `chatbox_v2` (
+  `user_id` int(20) NOT NULL,
   `username` varchar(30) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `reg_date` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `message` varchar(500) NOT NULL,
+  `ip_addr` varchar(255) NOT NULL,
+  `message_time` varchar(20) NOT NULL,
+  `message_id` int(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `userst`
+-- Indexes for table `chatbox_v2`
 --
-ALTER TABLE `userst`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `chatbox_v2`
+  ADD PRIMARY KEY (`message_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `userst`
+-- AUTO_INCREMENT for table `chatbox_v2`
 --
-ALTER TABLE `userst`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+ALTER TABLE `chatbox_v2`
+  MODIFY `message_id` int(20) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
